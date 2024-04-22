684e95a54969f3be18425ba2faa1d0ea5668b8c5
Imanol <imanolof29@gmail.com>
2024-04-22 17:55:08 +0200

    first commit

---
+++ src/test/java/com/imanolortiz/gamesapi/GamesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imanolortiz.gamesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
